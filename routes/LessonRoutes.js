07d773576690a9a772f02d549f1fea5da587a54e
siva shankari <sivas7773@gmail.com>
2025-10-07 21:48:00 +0530

    finish

--- routes/LessonRoutes.js
+++ routes/LessonRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const { CreateLesson, getLessons,getLessonsByCourseId, updateLesson, deleteLesson } = require("../controller/LessonController");
-const authMiddleware = require("../../Module5-Backend/middleware/authMiddleware");
+const authMiddleware = require("../middleware/authMiddleware");
 
 router.post("/:courseId",authMiddleware(["admin","instructor"]), CreateLesson);
 
